70a34ec68ef25c47fd106605ed98edd63637d35b
Alex Varatharajah <alex.varatharaj@onzo.com>
2019-04-10 12:22:29 +0100

    fixed typo for feature_name

--- {{cookiecutter.project_slug}}/analytics/{{cookiecutter.feature_name}}/tests/postgresql_resources/__init__.py
+++ {{cookiecutter.project_slug}}/analytics/{{cookiecutter.feature_name}}/tests/postgresql_resources/__init__.py
@@ -1,2 +1,2 @@
-from {{cookiecutter.project_slug}}.analytics.{{cookiecutter.featurename}}.tests.postgresql_test_infrastructure import postgresqlTestCase, _schema
+from {{cookiecutter.project_slug}}.analytics.{{cookiecutter.feature_name}}.tests.postgresql_test_infrastructure import postgresqlTestCase, _schema
 
