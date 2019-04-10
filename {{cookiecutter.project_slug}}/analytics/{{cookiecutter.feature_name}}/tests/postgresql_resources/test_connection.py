d623da870f8e37e80af111d7edde4ed8890b75ae
Alex Varatharajah <alex.varatharaj@onzo.com>
2019-04-10 12:31:07 +0100

    fixed a module path for postgres test infrastructure

--- {{cookiecutter.project_slug}}/analytics/{{cookiecutter.feature_name}}/tests/postgresql_resources/test_connection.py
+++ {{cookiecutter.project_slug}}/analytics/{{cookiecutter.feature_name}}/tests/postgresql_resources/test_connection.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 import pytest
-from {{cookiecutter.project_slug}}.analytics.{{cookiecutter.feature_name}}.postgresql_resources.postgresql_test_infrastructure import postgresqlTestCase
+from {{cookiecutter.project_slug}}.analytics.{{cookiecutter.feature_name}}.tests.postgresql_resources.postgresql_test_infrastructure import postgresqlTestCase
 
 
 class TestPG(unittest.TestCase):
